182c122d1c1d6a88e070360cdab21b337f806f0f
F7MH2R <skyicef7@gmail.com>
2024-04-14 14:53:38 -0600

    modificaciones login 2

--- Proyecto/snow_style_front/src/components/pages/Login.jsx
+++ Proyecto/snow_style_front/src/components/pages/Login.jsx
@@ -1,61 +1,87 @@
-import { useState } from "react";
-import "./css/Modal.css"
-//import logo from "./logo.svg";
-
- const Login = () => {
+import React, { useState } from "react";
+import "./css/Modal.css";
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import KeyboardTabOutlinedIcon from '@mui/icons-material/KeyboardTabOutlined';
+const Login = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const toggleModal = () => setIsOpen(!isOpen);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes manejar la lógica para enviar el formulario
+
+    // Limpia los campos de entrada después del envío del formulario
+    setEmail("");
+    setPassword("");
+
+    // Cierra el modal
+    closeModal();
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <section className="page modal-1-page">
       <div
         className={`modal-1-overlay ${isOpen ? "open" : ""}`}
-        onClick={toggleModal}
+        onClick={closeModal}
       >
-        <div className="modal-1-modal">
+        <div className="modal-1-modal" onClick={(e) => e.stopPropagation()}>
           <header>
             <h2>Sign Up</h2>
             <h3>SnowStyle</h3>
           </header>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="textbox">
-              <span class="material-symbols-outlined"></span>
-              <input type="email" placeholder="Email" />
+              <span className="material-symbols-outlined">
+                <EmailIcon />
+              </span>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="textbox">
-              <span class="material-symbols-outlined"></span>
-              <input type="password" placeholder="Password" />
+              <span className="material-symbols-outlined">
+                <LockIcon />
+              </span>
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                className="password-toggle"
+                type="button"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+              </button>
             </div>
-            <button
-              className="signup-button"
-              type="submit"
-              onClick={toggleModal}
-            >
-              <p></p>
-              <span className="material-symbols-outlined"> Inicar Sesión </span>
+            <button className="signup-button" type="submit">
+              <span className="material-symbols-outlined"> 
+              <KeyboardTabOutlinedIcon/>Iniciar Sesión </span>
             </button>
           </form>
           <p>No necesitas tarjeta de credito</p>
         </div>
       </div>
-      <footer className="modal-1-footer">
-        <div className="container">
-          <article>
-            <h2>Listo Para Iniciar.</h2>
-            <button
-              className="signup-button"
-              type="button"
-              onClick={toggleModal}
-            >
-              <p>Registrate Gratis</p>
-              <span className="material-symbols-outlined"> </span>
-            </button>
-          </article>
-          
-        </div>
-      </footer>
+     
     </section>
   );
 };
+
 export default Login;
