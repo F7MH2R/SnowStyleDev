d4ac81aaa28af001075dd9b892d0acf6463d9c09
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-04-14 23:16:59 -0600

    Ivonne: Resolviendo conflictos

--- Proyecto/snow_style_front/src/App.jsx
+++ Proyecto/snow_style_front/src/App.jsx
@@ -3,8 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Login from "./components/pages/Login";
 import Registrarse from "./components/pages/Register";
-import Login from "./components/pages/Login";
-import Registrarse from "./components/pages/Register";
 import Explorar from "./components/pages/Explorer";
 import Contact from "./components/pages/Contact";
 import Car from "./components/pages/Car";
