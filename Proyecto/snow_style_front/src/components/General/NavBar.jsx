214c428c9f8451b1263b994daca05c37d351e42b
Ivonne Duran <111211850+ivonne-duran@users.noreply.github.com>
2024-04-14 23:28:27 -0600

    Carrito de compras sin estilos (#3)
    * Ivonne: Version preliminar del carrito
    
    * Ivonne: Canasta de compra basica - sin estilos
    
    * Ivonne: Resolviendo conflictos

--- Proyecto/snow_style_front/src/components/General/NavBar.jsx
+++ Proyecto/snow_style_front/src/components/General/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
-import logo from "./img/Logo SnowStyle.PNG"
-import "../pages/css/Modal.css" // Importa el archivo de estilos CSS
+import logo from "./img/Logo SnowStyle.PNG";
+import "../pages/css/Modal.css"; // Importa el archivo de estilos CSS
 
 const NavBar = () => {
   const [mostrarBusqueda, setMostrarBusqueda] = useState(false); // Estado para controlar la visibilidad del área de búsqueda
@@ -14,29 +14,40 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <a href="#hombre" className="navbar-link">Hombre</a>
-        <a href="#mujer" className="navbar-link">Mujeres</a>
-        <a href="#niño" className="navbar-link">Niños</a>
+        <a href="#hombre" className="navbar-link">
+          Hombre
+        </a>
+        <a href="#mujer" className="navbar-link">
+          Mujeres
+        </a>
+        <a href="#niño" className="navbar-link">
+          Niños
+        </a>
       </div>
       <div className="navbar-brand">
         <img src={logo} alt="Logo de SnowStyle" className="logo" />
       </div>
-      <div className={`navbar-right ${mostrarBusqueda ? 'mostrar-busqueda' : ''}`}>
+      <div
+        className={`navbar-right ${mostrarBusqueda ? "mostrar-busqueda" : ""}`}
+      >
         <div className="buscar-icono" onClick={handleToggleBusqueda}>
           <FaSearch />
         </div>
         {mostrarBusqueda && (
-  <div className="area-busqueda-container">
-    <div className="area-busqueda">
-      <input type="text" placeholder="Escribe aquí para buscar..."  />
-      <button>Buscar</button>
-    </div>
-  </div>
-)}
+          <div className="area-busqueda-container">
+            <div className="area-busqueda">
+              <input type="text" placeholder="Escribe aquí para buscar..." />
+              <button>Buscar</button>
+            </div>
+          </div>
+        )}
 
-
-        <a href="#carrito" className="navbar-link"><FaShoppingCart /></a>
-        <a href="#usuario" className="navbar-link"><FaUser /></a>
+        <a href="/cart" className="navbar-link">
+          <FaShoppingCart />
+        </a>
+        <a href="#usuario" className="navbar-link">
+          <FaUser />
+        </a>
       </div>
     </div>
   );
---
+++ Proyecto/snow_style_front/src/components/Item/Item.jsx
@@ -0,0 +1,27 @@
+import react from "react";
+import "./Item.css";
+const Item = ({ imagen, descripcion, precio, id }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col imagen-small">
+          {imagen ? (
+            <img className="imagen-small" src={imagen} />
+          ) : (
+            <div>No imagen</div>
+          )}
+        </div>
+        <div className="col">
+          <div className="row">$ {precio}</div>
+          <div className="row">{descripcion}</div>
+        </div>
+        <div className="col">cantidad</div>
+        <div className="col">
+          <a href={`eliminar/${id}`}>Eliminar</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
