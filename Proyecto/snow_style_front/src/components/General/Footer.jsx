fde43eed92812465baaad13d208fc41d586c5aff
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2024-04-14 21:50:53 -0600

    oa

--- Proyecto/snow_style_front/src/components/General/Footer.jsx
+++ Proyecto/snow_style_front/src/components/General/Footer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-const Footer=()=>{
-    return(
-        <div><h1>footer</h1></div>
+import "../pages/css/Modal.css" 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTiktok, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="redes-sociales">
+          {/* Botones de redes sociales */}
+          <button><FontAwesomeIcon icon={faFacebook} /></button>
+          <button><FontAwesomeIcon icon={faTiktok} /></button>
+          <button><FontAwesomeIcon icon={faTwitter} /></button>
+          <button><FontAwesomeIcon icon={faInstagram} /></button>
+        </div>
+        <div className="secciones">
+          {/* Sección de Copyright */}
+          <div className="seccion">
+            <p>© Snow Style 2024</p>
+          </div>
+          {/* Sección de Conócenos */}
+          <div className="seccion">
+            <h2>Conócenos</h2>
+            <p>Aquí puedes encontrar información sobre nuestra empresa y nuestra historia.</p>
+          </div>
+          {/* Sección de Contacto */}
+          <div className="seccion">
+            <h2>Contacto</h2>
+            <p>Si tienes alguna pregunta o quieres contactarnos, no dudes en escribirnos a nuestro correo electrónico o llamarnos.</p>
+          </div>
+          {/* Sección de Snow Style */}
+          <div className="seccion">
+            <h2>Snow Style</h2>
+            <p>Explora nuestro catálogo de productos y descubre lo último en moda de invierno.</p>
+          </div>
+        </div>
+      </footer>
     );
-};
-export default Footer;
+  };
+  
+  export default Footer;
--- Proyecto/snow_style_front/src/components/General/NavBar.jsx
+++ Proyecto/snow_style_front/src/components/General/NavBar.jsx
@@ -1,7 +1,45 @@
-import React from "react";
-const NavBar=()=>{
-    return(
-        <div><h1>NAV</h1></div>
-    );
+import React, { useState } from "react";
+import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "./img/Logo SnowStyle.PNG"
+import "../pages/css/Modal.css" // Importa el archivo de estilos CSS
+
+const NavBar = () => {
+  const [mostrarBusqueda, setMostrarBusqueda] = useState(false); // Estado para controlar la visibilidad del área de búsqueda
+
+  // Función para manejar el clic en el icono de búsqueda
+  const handleToggleBusqueda = () => {
+    setMostrarBusqueda(!mostrarBusqueda); // Cambia el estado para mostrar u ocultar el área de búsqueda
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <a href="#hombre" className="navbar-link">Hombre</a>
+        <a href="#mujer" className="navbar-link">Mujeres</a>
+        <a href="#niño" className="navbar-link">Niños</a>
+      </div>
+      <div className="navbar-brand">
+        <img src={logo} alt="Logo de SnowStyle" className="logo" />
+      </div>
+      <div className={`navbar-right ${mostrarBusqueda ? 'mostrar-busqueda' : ''}`}>
+        <div className="buscar-icono" onClick={handleToggleBusqueda}>
+          <FaSearch />
+        </div>
+        {mostrarBusqueda && (
+  <div className="area-busqueda-container">
+    <div className="area-busqueda">
+      <input type="text" placeholder="Escribe aquí para buscar..."  />
+      <button>Buscar</button>
+    </div>
+  </div>
+)}
+
+
+        <a href="#carrito" className="navbar-link"><FaShoppingCart /></a>
+        <a href="#usuario" className="navbar-link"><FaUser /></a>
+      </div>
+    </div>
+  );
 };
+
 export default NavBar;
